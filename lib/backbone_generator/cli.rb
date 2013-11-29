389f2da1e720b0f93722c49de69deb63336359f1
mhdtahawi <mhd.tahawi@gmail.com>
2013-11-29 06:25:29 +0200

    CLI initial commit

---
+++ lib/backbone_generator/cli.rb
@@ -0,0 +1,21 @@
+require 'thor'
+module backbone_generator
+	class	CLI < Thor
+
+		def new app_name
+		end
+		def model model_name
+		end
+		def view view_name
+		end
+		def route route_name
+		end
+		def collection collection_name
+		end
+		def utility utility_name
+		end
+		def library library_name
+		end
+		
+	end
+end

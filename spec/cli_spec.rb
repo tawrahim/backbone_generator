2dd307136952da22d78f8b4ed3391df9a209b9fd
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-11-30 12:02:55 -0500

    more cli test

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -7,6 +7,8 @@ describe BackboneGenerator::CLI do
         @cli = BackboneGenerator::CLI.new
     end
 
+    subject { @cli }
+
     it { should respond_to(:new) }
     it { should respond_to(:model) }
     it { should respond_to(:view) }
@@ -15,6 +17,12 @@ describe BackboneGenerator::CLI do
     it { should respond_to(:utility) }
     it { should respond_to(:library) }
 
-    describe "when new does not have an argument" do
+    it { should be_an_instance_of(BackboneGenerator::CLI) }
+    it { should be_kind_of(Thor) }
+    
+    it { should_receive(:new).with(:app_name) } 
+    
+    describe "the instance methods" do
+        its(:foo) { should == 'Bar' }
     end
 end

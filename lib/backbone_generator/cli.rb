0dda21a0c3b57c7c4ffbddf9d4949628815d4733
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-12-01 15:47:23 -0500

    refactor cli code

--- lib/backbone_generator/cli.rb
+++ lib/backbone_generator/cli.rb
@@ -1,52 +1,73 @@
 require 'thor'
-require 'auxilliary'
+require 'backbone_generator/auxilliary'
 
 module BackboneGenerator
     class	CLI < Thor
 
+        private
+        # isvalid? is invked before cn action
+        # check if procedding with the action is vaild, if not
+        # print a message telling what is wrong
+        # @author Mhd Tahawi 
+        def isvalid?
+            # This line is neccesary because if the user invokes
+            # the other method before invoking init it will blow up
+            @app_directory ||= ""
+            if @app_directory.empty?
+               puts "create an app first"
+               false
+            else
+                puts "valid"
+                true
+            end
+        end
+
+        public
         desc "new app_name", "Create a new backbone.js application named app_name"
         def new app_name
+            # assign current directory the dir of the application
+            @app_directory = File.expand_path(app_name, __FILE__)
         end
 
         desc "model model_name", "Create a new backbone.js model named model_name "
         def model model_name
-            BackboneGenerator.inside_backbone_generator_app? ? puts "wrong" : puts "correct"
+            puts "creating a model" unless !isvalid?
         end
 
         desc "view view_name", "Create a new backbone.js view named view_name"
         def view view_name
-            BackboneGenerator.inside_backbone_generator_app? ? puts "wrong" : puts "correct"
+            puts "creating a model" unless !isvalid?
         end
 
         desc "route route_name", "Create a new backbone.js route named route_name"
         def route route_name
-            BackboneGenerator.inside_backbone_generator_app? ? puts "wrong" : puts "correct"
+            puts "creating a model" unless !isvalid?
         end
 
         desc " collection collection_name", "Create a new backbone.js collection named collection_name"	
         def collection collection_name
-            BackboneGenerator.inside_backbone_generator_app? ? puts "wrong" : puts "correct"
+            puts "creating a model" unless !isvalid?
         end
 
 
         desc "utility utility_name", "Create a new backbone.js utility  named utility_name"
         def utility utility_name
-            BackboneGenerator.inside_backbone_generator_app? ? puts "wrong" : puts "correct"
+            puts "creating a model" unless !isvalid?
         end
-        
+
         desc "template template_name", "Create a template associated with a view"
         def template template_name
-            BackboneGenerator.inside_backbone_generator_app? ? puts "wrong" : puts "correct"
+            puts "creating a model" unless !isvalid?
         end
-        
+
         desc "library library_name", "Create a new backbone.js library named library_name"
         def library library_name
-            BackboneGenerator.inside_backbone_generator_app? ? puts "wrong" : puts "correct"
+            puts "creating a model" unless !isvalid?
         end
 
         desc "test test_name", "Create a new jasmine test"
         def test test_name
-            BackboneGenerator.inside_backbone_generator_app? ? puts "wrong" : puts "correct"
+            puts "creating a model" unless !isvalid?
         end
 
     end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -9,6 +9,9 @@ describe BackboneGenerator::CLI do
 
     subject { @cli }
 
+    it { should be_an_instance_of(BackboneGenerator::CLI) }
+    it { should be_kind_of(Thor) }
+
     it { should respond_to(:new) }
     it { should respond_to(:model) }
     it { should respond_to(:view) }
@@ -16,10 +19,16 @@ describe BackboneGenerator::CLI do
     it { should respond_to(:collection) }
     it { should respond_to(:utility) }
     it { should respond_to(:library) }
+    it { should respond_to(:template) }
+    it { should respond_to(:test) }
 
-    it { should be_an_instance_of(BackboneGenerator::CLI) }
-    it { should be_kind_of(Thor) }
-    
-    it { should_receive(:new).with(:app_name) } 
-    
+    describe "#new" do
+       it "should accept :app_name as a parameter" do
+           expect { @cli.new(:app_name) }.to_not raise_error
+       end
+
+       it "should not accept two args" do
+           expect { @cli.new(:foo, :bar) }.to raise_error
+       end
+    end
 end
